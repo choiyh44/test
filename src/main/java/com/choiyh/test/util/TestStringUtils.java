114f78d11c135e94d73e770be1965c6079ad758f
최영환[네무소프트] <ne90418@nhnent.com>
2015-07-15 09:46:54 +0900

    springframwork StringUtils 테스트

---
+++ src/main/java/com/choiyh/test/util/TestStringUtils.java
@@ -0,0 +1,17 @@
+package com.choiyh.test.util;
+
+import org.springframework.util.StringUtils;
+
+public class TestStringUtils {
+	public static void main(String[] args) throws Exception {
+		String resultStr;
+		String testStr = "    1234567890    ";
+		String nullStr = null;
+
+		resultStr = StringUtils.trimWhitespace(testStr);
+		System.out.println("[" + resultStr + "]");
+		resultStr = StringUtils.trimWhitespace(nullStr);
+		System.out.println("[" + resultStr + "]");
+	}
+
+}
